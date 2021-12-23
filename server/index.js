b618d122a353205ee1236c58d26186a325ee0b0d
Joseph Lee <teddylou0215@gmail.com>
2021-12-23 20:36:42 +0000

    Update data.sql for seed data for Gordon Ramsay profile. Include name, photoUrl, and bio.#2

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const { json } = require('express');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
+
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.get('/api/chefs', (req, res) => {
+  const sql = `
+    select *
+    from "chefs"
+  `
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows)
+    })
+    .catch(err => next(err))
+})
 
 app.use(staticMiddleware);
 
