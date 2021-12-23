512e996cb4bb40c6b80e94712fad6f48bb3fbe20
Joseph Lee <teddylou0215@gmail.com>
2021-12-23 21:00:27 +0000

    Create a GET route to get a chef by id.#3

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
-const { json } = require('express');
+// const { json } = require('express');
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -18,19 +18,34 @@ const jsonMiddleware = express.json();
 
 app.use(jsonMiddleware);
 
-app.get('/api/chefs', (req, res) => {
+// app.get('/api/chefs', (req, res) => {
+//   const sql = `
+//     select *
+//     from "chefs"
+//   `
+//   db.query(sql)
+//     .then(result => {
+//       res.json(result.rows)
+//     })
+//     .catch(err => next(err))
+// })
+
+app.use(staticMiddleware);
+
+app.get('/api/chefs/:chefId', (req, res) => {
+  const chefId = Number(req.params.chefId);
   const sql = `
     select *
     from "chefs"
-  `
-  db.query(sql)
+    where "chefId" = $1
+  `;
+  const params = [chefId];
+  db.query(sql, params)
     .then(result => {
-      res.json(result.rows)
+      res.json(result.rows);
     })
-    .catch(err => next(err))
-})
-
-app.use(staticMiddleware);
+    .catch(err => console.error(err));
+});
 
 app.use(errorMiddleware);
 
