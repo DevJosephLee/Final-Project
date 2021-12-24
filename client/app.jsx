b0dc48606b1891218bcd6dae0ce9e375a83184fe
Joseph Lee <teddylou0215@gmail.com>
2021-12-24 00:33:47 +0000

    save doc

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import Home from './pages/home';
+// import Home from './pages/home';
+import ChefProfile from './pages/chef-profile';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chefs: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('api/chefs')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ chefs: data });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   render() {
-    return <Home />;
+    return <ChefProfile />;
   }
 }
