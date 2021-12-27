fc8091d3cc40d5a4ec084bc3d492fba7eabf4806
Joseph Lee <teddylou0215@gmail.com>
2021-12-27 22:03:46 +0000

    Create chef.jsx for a page dedicated to chef's profile page. Moved code from app.jsx to chef.jsx and app.jsx now returns chef.jsx component

--- client/app.jsx
+++ client/app.jsx
@@ -1,42 +1,10 @@
 import React from 'react';
-import ProfilePicture from './pages/profile-picture';
-import ProfileName from './pages/profile-name';
-import DishPictures from './pages/dish-pictures';
+import ChefProfile from './pages/chef';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chef: []
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api/chefs/1')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ chef: data });
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
-
-  render() {
-    return (
-      <div className='container'>
-        {
-          this.state.chef.map(chef => {
-            return (
-              <div key={chef.name} className='mobile-row text-align-center padding-top-bottom'>
-                <ProfilePicture photoUrl={chef.photoUrl} />
-                <ProfileName name={chef.name} />
-              </div>
-            );
-          })
-        }
-        <DishPictures />
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <ChefProfile />
+  );
 }
+
+export default App;
