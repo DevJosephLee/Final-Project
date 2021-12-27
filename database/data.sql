010d0fd148ad25324fdb2002a5c703b69f6cf691
Joseph Lee <teddylou0215@gmail.com>
2021-12-27 22:49:43 +0000

    Update data.sql to add seed data for users and reviews.#1

--- database/data.sql
+++ database/data.sql
@@ -6,3 +6,9 @@ values (1, 'Tomato Spinach Pasta', 'images/pasta.jpeg'),
        (1, 'Cheeseburger', 'images/Cheeseburger.jpeg'),
        (1, 'Tofu & Watercress Salad', 'images/salad.jpeg'),
        (1, 'French Onion Soup', 'images/soup.jpeg')
+
+insert into "users" ("username", "password", "createdAt")
+values('User1', 'testing123', current_timestamp)
+
+insert into "reviews" ("userId", "chefId", "content", "rating", "createdAt")
+values (3, 1, 'testing', 5, current_timestamp)
