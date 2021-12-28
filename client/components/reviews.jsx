524c5785f1566a38a9c86047429eb80929960b64
Joseph Lee <teddylou0215@gmail.com>
2021-12-28 01:27:05 +0000

    Final Cleanup

--- client/components/reviews.jsx
+++ client/components/reviews.jsx
@@ -22,27 +22,25 @@ class Reviews extends React.Component {
     return (
       <div className='width-adj padding-bottom'>
         <h1>Reviews</h1>
-        <div className='align-center'>
-          {
-            this.state.reviews.map(reviews => {
-              return (
-                <div key={reviews.reviewId}>
-                  <div className='row align-center'>
-                    <div className='row user-icon-container align-center justify-center margin-right'>
-                      <i className='far fa-grin'></i>
-                    </div>
-                    <div>
-                      <h3>{reviews.username}</h3>
-                      <h3>{reviews.createdAt.slice(0, 10)}</h3>
-                      <StarRating rating={reviews.rating} />
-                    </div>
+        {
+          this.state.reviews.map(reviews => {
+            return (
+              <div key={reviews.reviewId}>
+                <div className='row align-center'>
+                  <div className='row user-icon-container align-center justify-center margin-right'>
+                    <i className='far fa-grin'></i>
                   </div>
-                  <p className='review-content'>{reviews.content}</p>
+                  <div>
+                    <h3>{reviews.username}</h3>
+                    <h3>{reviews.createdAt.slice(0, 10)}</h3>
+                    <StarRating rating={reviews.rating} />
+                  </div>
                 </div>
-              );
-            })
-          }
-        </div>
+                <p className='review-content'>{reviews.content}</p>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
