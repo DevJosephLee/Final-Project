90845735ba902607c27702cc0dded2c2149aa0b6
Joseph Lee <teddylou0215@gmail.com>
2021-12-29 23:16:31 +0000

    Use jwt.verify() to verify the authenticity of the token and extract its payload.#15

--- server/authorization-middleware.js
+++ server/authorization-middleware.js
@@ -2,10 +2,11 @@ const jwt = require('jsonwebtoken');//eslint-disable-line
 const ClientError = require('./client-error');//eslint-disable-line
 
 function authorizationMiddleware(req, res, next) {
-  const token = req.get('X-Access-Token');//eslint-disable-line
+  const token = req.get('X-Access-Token');
   if (!token) {
     throw new ClientError(401, 'authentication required');
   }
+  const payload = jwt.verify(token, process.env.TOKEN_SECRET);//eslint-disable-line
 }
 
 export default authorizationMiddleware;
