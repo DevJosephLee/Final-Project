917823fb170d9b04296f2da55feca916be2bf85b
Joseph Lee <teddylou0215@gmail.com>
2021-12-29 23:13:33 +0000

    Get the 'X-Access-Token' from the request headers, and assign the return to variable 'token'#13

--- server/authorization-middleware.js
+++ server/authorization-middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');//eslint-disable-line
 const ClientError = require('./client-error');//eslint-disable-line
 
 function authorizationMiddleware(req, res, next) {
-
+  const token = req.get('X-Access-Token');//eslint-disable-line
 }
 
 export default authorizationMiddleware;
