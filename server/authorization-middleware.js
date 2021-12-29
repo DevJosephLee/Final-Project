34c06c49c13bdb70df4ee887e686e8ab0b06f9b9
Joseph Lee <teddylou0215@gmail.com>
2021-12-29 23:14:47 +0000

    If no token is provided, throw a 401 error with the message 'authentication required'#14

--- server/authorization-middleware.js
+++ server/authorization-middleware.js
@@ -3,6 +3,9 @@ const ClientError = require('./client-error');//eslint-disable-line
 
 function authorizationMiddleware(req, res, next) {
   const token = req.get('X-Access-Token');//eslint-disable-line
+  if (!token) {
+    throw new ClientError(401, 'authentication required');
+  }
 }
 
 export default authorizationMiddleware;
