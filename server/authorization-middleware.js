15234520d616dd1fcab02701d9a37e2c4c50882c
Joseph Lee <teddylou0215@gmail.com>
2021-12-29 23:10:41 +0000

    In authorization-middleware.jsx, require 'jsonwebtoken'#12

--- server/authorization-middleware.js
+++ server/authorization-middleware.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');//eslint-disable-line
+const ClientError = require('./client-error');//eslint-disable-line
 
 function authorizationMiddleware(req, res, next) {
 
