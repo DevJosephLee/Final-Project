059610e7030803e80fb9df865a366651a2a01469
Joseph Lee <teddylou0215@gmail.com>
2021-12-29 19:35:53 +0000

    set up hashroute for sign-in page

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import ChefProfile from './pages/chef';
-import SignInPage from './pages/sign-in';
+import SignUpPage from './pages/sign-up';
 import NavBar from './components/nav-bar';
+import parseRoute from './lib/parse-route';
+import SignInPage from './pages/sign-in';
 
-function App(props) {
-  return (
-    <div>
-      <NavBar />
-      <SignInPage />
-      <ChefProfile />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '') {
+      return <SignUpPage />;
+    }
+    if (route.path === 'sign-in') {
+      return <SignInPage />;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        {this.renderPage()}
+        <ChefProfile />
+      </div>
+    );
+  }
 }
+
+// function App(props) {
+//   return (
+//     <div>
+//       <NavBar />
+//       <SignUpPage />
+//       <ChefProfile />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ client/pages/sign-up.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import SignupForm from '../components/signup-form';
+
+class SignUpPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+    this.addNewUser = this.addNewUser.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/users')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ users: data });
+      })
+      .catch(err => console.error(err));
+  }
+
+  addNewUser(newUser) {
+    fetch('/api/auth/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(response => response.json())
+      .then(newUser => {
+        this.setState({ users: [].concat(this.state.users, newUser) });
+      })
+      .catch(err => console.error(err));
+    window.location.hash = 'sign-in';
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-align-center padding-top-bottom-3">Sign-Up</h1>
+        <SignupForm onSubmit={this.addNewUser} />
+      </div>
+    );
+  }
+}
+
+export default SignUpPage;
