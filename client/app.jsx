e56e7105491e4460e51dd8438e3742a86b18b66c
Joseph Lee <teddylou0215@gmail.com>
2021-12-30 18:52:41 +0000

    Refactor sign-in.jsx and sign-up.jsx to auth.jsx. Refactor signup-form.jsx to auth-form.jsx. Create redirect.jsx, app-context.js, decode-token.js.

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ChefProfile from './pages/chef';
-import SignUpPage from './pages/sign-up';
-import NavBar from './components/nav-bar';
+import AppContext from './lib/app-context';
 import parseRoute from './lib/parse-route';
-import SignInPage from './pages/sign-in';
+import decodeToken from './lib/decode-token';
+import Auth from './pages/auth';
+import ChefProfile from './pages/chef';
+import NavBar from './components/nav-bar';
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,33 +14,50 @@ class App extends React.Component {
       isAuthorizing: true,
       route: parseRoute(window.location.hash)
     };
+    this.handleSignIn = this.handleSignIn.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener('hashchange', () => {
-      this.setState({ route: parseRoute(window.location.hash) });
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
     });
+    const token = window.localStorage.getItem('final-project-jwt');
+    const user = token ? decodeToken(token) : null;
+    this.setState({ user, isAuthorizing: false });
+  }
+
+  handleSignIn(result) {
+    const { user, token } = result;
+    window.localStorage.setItem('final-project-jwt', token);
+    this.setState({ user });
   }
 
   renderPage() {
-    const { route } = this.state;
-    if (route.path === '') {
-      return <SignUpPage />;
+    const { path } = this.state.route;
+    if (path === '') {
+      return <ChefProfile />;
     }
-    if (route.path === 'sign-in') {
-      return <SignInPage />;
+    if (path === 'sign-in' || path === 'sign-up') {
+      return <Auth />;
     }
   }
 
   render() {
+    if (this.state.isAuthorizing) return null;
+    const { user, route } = this.state;
+    const { handleSignIn, handleSignOut } = this;
+    const contextValue = { user, route, handleSignIn, handleSignOut };
     return (
-      <div>
-        <NavBar />
-        {this.renderPage()}
-        <ChefProfile />
+      <div className="">
+        <AppContext.Provider value={contextValue}>
+          <>
+            <NavBar />
+            {this.renderPage()}
+          </>
+        </AppContext.Provider>
       </div>
     );
   }
 }
-
-export default App;
---
+++ client/components/auth-form.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import AppContext from '../lib/app-context';
+
+export default class AuthForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { action } = this.props;
+    fetch(`/api/auth/${action}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(res => res.json())
+      .then(result => {
+        if (action === 'sign-up') {
+          window.location.hash = 'sign-in';
+        } else if (result.user && result.token) {
+          this.props.onSignIn(result);
+        }
+      });
+  }
+
+  render() {
+    const { action } = this.props;
+    const { handleChange, handleSubmit } = this;
+    const alternateActionHref = action === 'sign-up'
+      ? '#sign-in'
+      : '#sign-up';
+    const alternateActionText = action === 'sign-up'
+      ? 'Sign in instead'
+      : 'Register now';
+    const alternateActionDesc = action === 'sign-up'
+      ? 'Already have an account?'
+      : "Don't have an account?";
+    const submitButtonText = action === 'sign-up'
+      ? 'Register'
+      : 'Log In';
+    return (
+      <div className="container">
+        <div className="row justify-center padding-bottom-2">
+          <form onSubmit={handleSubmit}>
+            <div className='margin-bottom'>
+              <label className="label-signin" htmlFor='username'>Username</label>
+              <div>
+                <input
+                  className="inputs-signin"
+                  required
+                  autoFocus
+                  type="text"
+                  name="username"
+                  onChange={handleChange}
+                  id='username' />
+              </div>
+            </div>
+            <div className="margin-bottom">
+              <label className="label-signin" htmlFor='password'>Password</label>
+              <div>
+                <input
+                  className="inputs-signin"
+                  required
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  id='password' />
+              </div>
+            </div>
+            <div className="row justify-between align-center padding-top-bottom-4">
+              <p>
+                {alternateActionDesc}&nbsp;&nbsp;
+                <a href={alternateActionHref}>
+                  {alternateActionText}
+                </a>
+              </p>
+              <button className='submit-buttom' type="submit">{submitButtonText}</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+AuthForm.ContextType = AppContext;
