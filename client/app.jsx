6ec7913a32d208332700665ce0b71ef844fb475c
Joseph Lee <teddylou0215@gmail.com>
2022-01-04 05:46:24 +0000

    Set hash routing to sign in page when user logs out.#5

--- client/app.jsx
+++ client/app.jsx
@@ -41,6 +41,7 @@ export default class App extends React.Component {
   handleSignOut() {
     window.localStorage.removeItem('final-project-token');
     this.setState({ user: null });
+    window.location.hash = 'sign-in';
   }
 
   handleProfileClick() {
