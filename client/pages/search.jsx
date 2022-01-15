3dece1e375b8c9fb0c64ced76d73badbfe64e8b8
Joseph Lee <teddylou0215@gmail.com>
2022-01-15 00:52:45 +0000

    fix search button on search page

--- client/pages/search.jsx
+++ client/pages/search.jsx
@@ -52,7 +52,7 @@ class SearchPage extends React.Component {
                   </div>
                 </div>
                 <div className="d-flex justify-content-center">
-                  <div className="col-lg-2 col-sm-3">
+                  <div className="col-3">
                     <button className="w-100 mt-4 mb-4 btn btn-primary" type="submit">SEARCH</button>
                   </div>
                 </div>
