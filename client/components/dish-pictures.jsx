3a84ddf955f6d2f0b1baff2852d945ce6bc8b110
Joseph Lee <teddylou0215@gmail.com>
2022-04-26 03:02:20 +0000

    remove unncessary code

--- client/components/dish-pictures.jsx
+++ client/components/dish-pictures.jsx
@@ -4,8 +4,7 @@ class DishPictures extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: [],
-      currentImageIndex: 0
+      dishes: []
     };
   }
 
@@ -19,12 +18,6 @@ class DishPictures extends React.Component {
   }
 
   render() {
-    // const dishPhotoUrls = [];
-    // const dishNames = [];
-    // for (let i = 0; i < this.state.dishes.length; i++) {
-    //   dishPhotoUrls.push(this.state.dishes[i].photoUrl);
-    //   dishNames.push(this.state.dishes[i].name);
-    // }
     return (
       <div>
         <h1>Dishes</h1>
@@ -32,30 +25,6 @@ class DishPictures extends React.Component {
           <div className="text-center">
             <div id="carouselIndicators" className="carousel slide" data-ride="carousel">
               <div className="carousel-inner">
-                {/* <div className="carousel-item active">
-                  <img className="d-block w-100 dish-photos" src={dishPhotoUrls[0]} alt={dishNames[0]} />
-                  <div className="mb-5">
-                    <p>{dishNames[0]}</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img className="d-block w-100 dish-photos" src={dishPhotoUrls[1]} alt={dishNames[1]} />
-                  <div className="mb-5">
-                    <p>{dishNames[1]}</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img className="d-block w-100 dish-photos" src={dishPhotoUrls[2]} alt={dishNames[2]} />
-                  <div className="mb-5">
-                    <p>{dishNames[2]}</p>
-                  </div>
-                </div>
-                <div className="carousel-item">
-                  <img className="d-block w-100 dish-photos" src={dishPhotoUrls[3]} alt={dishNames[3]} />
-                  <div className="mb-5">
-                    <p>{dishNames[3]}</p>
-                  </div>
-                </div> */}
                 {
                   this.state.dishes.map((dish, i) => {
                     return (
@@ -79,10 +48,6 @@ class DishPictures extends React.Component {
                 <span className="sr-only">Next</span>
               </a>
               <ol className="carousel-indicators gap-2">
-                {/* <li data-target="#carouselIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                <li data-target="#carouselIndicators" data-slide-to="3"></li> */}
                 {
                   this.state.dishes.map((dish, i) => {
                     return (
