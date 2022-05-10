7dea3fa2ea6ccc1f55a1103840b2aa6d1aa2a8c0
Joseph Lee <teddylou0215@gmail.com>
2022-05-10 19:51:33 +0000

    configure styling and alignment for ChatRoom component

--- client/components/chat-room.jsx
+++ client/components/chat-room.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import decodeToken from '../lib/decode-token';
 
 class ChatRoom extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentMessage: ''
+      currentMessage: '',
+      messageList: [],
+      username: ''
     };
     this.handleMessageChange = this.handleMessageChange.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
   }
 
   componentDidMount() {
+    const token = window.localStorage.getItem('user-jwt');
+    this.setState({ username: decodeToken(token).username });
     this.props.socket.on('receive_message', data => {
-      // eslint-disable-next-line no-console
-      console.log(data);
+      this.setState({ messageList: [].concat(this.state.messageList, data) });
     });
   }
 
@@ -27,30 +31,40 @@ class ChatRoom extends React.Component {
         roomId: this.props.roomId,
         author: this.props.username,
         message: this.state.currentMessage,
-        // time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes()
-        time: new Date()
+        time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes() + ':' + new Date(Date.now()).getSeconds()
       };
       this.props.socket.emit('send_message', messageData);
+      this.setState({ messageList: [].concat(this.state.messageList, messageData) });
     }
   }
 
   render() {
     return (
-    // <div>
-    //   <div className='chat-header'>
-    //     <p>Live Chat</p>
-    //   </div>
-    //   <div className='chat-body'>
-
-      //   </div>
-      //   <div className='chat-footer'>
-      //     <input type='text' onChange={this.handleMessageChange}/>
-      //     <button onClick={this.sendMessage}>&#9658;</button>
-      //   </div>
-      // </div>
       <div>
         <div className="chat-room-container">
-          <p>hello</p>
+          {
+            this.state.messageList.map(messageContent => {
+              return (
+                <div key={messageContent.time}>
+                  <div
+                  className={messageContent.author === this.state.username ? 'd-flex justify-content-end' : 'd-flex justify-content-start'}
+                  >
+                    <div
+                    className={messageContent.author === this.state.username ? 'message-content bg-primary' : 'message-content bg-success'}
+                    >
+                      <p className='fs-4'>{messageContent.message}</p>
+                    </div>
+                  </div>
+                  <div
+                    className={messageContent.author === this.state.username ? 'd-flex justify-content-end fst-italic' : 'd-flex justify-content-start fst-italic'}
+                  >
+                    <p>{messageContent.time}&nbsp;{messageContent.author}</p>
+                    <p></p>
+                  </div>
+                </div>
+              );
+            })
+          }
         </div>
         <div className="send-message-container">
           <input className="w-75" type="text" onChange={this.handleMessageChange}></input>
