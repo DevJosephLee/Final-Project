47f89f67e77cb149ff6ea2784b57c66d0ef7080a
Joseph Lee <teddylou0215@gmail.com>
2022-05-27 23:51:48 +0000

    configure ChatRoom component to fetch message data when clicking message button in ChefPage component

--- client/components/chat-room.jsx
+++ client/components/chat-room.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import decodeToken from '../lib/decode-token';
 import ScrollToBottom from 'react-scroll-to-bottom';
+import parseRoute from '../lib/parse-route';
 
 class ChatRoom extends React.Component {
   constructor(props) {
@@ -8,7 +9,10 @@ class ChatRoom extends React.Component {
     this.state = {
       currentMessage: '',
       username: '',
-      messageList: []
+      route: parseRoute(window.location.hash),
+      messageList: [],
+      tempMessageId: 0
+      // roomId: this.props.roomId
     };
     this.handleMessageChange = this.handleMessageChange.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
@@ -16,11 +20,47 @@ class ChatRoom extends React.Component {
   }
 
   componentDidMount() {
+    const { route } = this.state;
+    const chefId = route.params.get('chefId');
     const token = window.localStorage.getItem('user-jwt');
     this.setState({ username: decodeToken(token).username });
     this.props.socket.on('receive_message', data => {
       this.setState({ messageList: [].concat(this.state.messageList, data) });
     });
+    fetch('/api/getChatRoom/', {
+      headers: {
+        'X-Access-Token': token
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].chefId === Number(chefId)) {
+            fetch(`/api/messages/${data[i].roomId}`)
+              .then(response => response.json())
+              .then(messages => {
+                this.setState({ messageList: messages });
+              })
+              .catch(err => console.error(err));
+          }
+        }
+      })
+      .catch(err => console.error(err));
+    // fetch(`/api/messages/${txhis.state.roomId}`, {
+    // })
+    //   .then(response => response.json())
+    //   .then(messages => {
+    //     this.setState({ messageList: messages });
+    //   })
+    //   .catch(err => console.error(err));
+    // fetch(`/api/messages/${this.state.roomId}`, {
+    // })
+    //   .then(response => response.json())
+    //   .then(messages => {
+    //     this.setState({ messageList: messages });
+    //   })
+    //   .catch(err => console.error(err));
+    // console.log(this.props.roomId);
   }
 
   handleMessageChange(event) {
@@ -33,7 +73,8 @@ class ChatRoom extends React.Component {
         roomId: this.props.roomId,
         author: this.props.username,
         message: this.state.currentMessage,
-        time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes() + ':' + new Date(Date.now()).getSeconds()
+        messageId: `#${this.state.tempMessageId}`
+        // time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes() + ':' + new Date(Date.now()).getSeconds()
       };
       this.props.socket.emit('send_message', messageData);
       this.setState({ messageList: [].concat(this.state.messageList, messageData) });
@@ -58,6 +99,7 @@ class ChatRoom extends React.Component {
         console.error(err);
       });
     this.setState({ currentMessage: '' });
+    this.setState({ tempMessageId: this.state.tempMessageId + 1 });
   }
 
   render() {
@@ -67,21 +109,15 @@ class ChatRoom extends React.Component {
           {
             this.state.messageList.map(messageContent => {
               return (
-                <div key={messageContent.time}>
-                  <div
-                  className={messageContent.author === this.state.username ? 'd-flex justify-content-end' : 'd-flex justify-content-start'}
-                  >
-                    <div
-                    className={messageContent.author === this.state.username ? 'message-content bg-primary' : 'message-content bg-success'}
-                    >
+                <div key={messageContent.messageId}>
+                  <div className={messageContent.author === this.state.username ? 'd-flex justify-content-end' : 'd-flex justify-content-start'}>
+                    <div className={messageContent.author === this.state.username ? 'message-content bg-primary' : 'message-content bg-success'}>
                       <p>{messageContent.message}</p>
                     </div>
                   </div>
-                  <div
-                    className={messageContent.author === this.state.username ? 'd-flex justify-content-end fst-italic' : 'd-flex justify-content-start fst-italic'}
-                  >
-                    <p>{messageContent.time}&nbsp;{messageContent.author}</p>
-                    <p></p>
+                  <div className={messageContent.author === this.state.username ? 'd-flex justify-content-end fst-italic' : 'd-flex justify-content-start fst-italic'}>
+                    <p>{messageContent.author}</p>
+                    {/* {messageContent.time}&nbsp; */}
                   </div>
                 </div>
               );
